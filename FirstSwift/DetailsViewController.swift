1d301fa231b97da78c7e2c9e74a1efe933acc793
Rathish <rathish_citys@eres.ae>
2015-12-21 13:58:06 +0400

    Added search bar

--- FirstSwift/DetailsViewController.swift
+++ FirstSwift/DetailsViewController.swift
@@ -11,12 +11,19 @@ import UIKit
 
 let  ROOT_WEB_URL = "http://devmobile.dubailand.gov.ae:4000/tuo.Mobile.Server/api/tuo?CustomerId=10662&&custstartindex=0&custrowscounts=20"
 
-class DetailsViewController: UIViewController, UITableViewDataSource, DLParserManagerDelegate {
+class DetailsViewController: UIViewController, UITableViewDataSource, DLParserManagerDelegate, UISearchBarDelegate, CustomSearchControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
     var arrData = NSMutableArray()
     let managerParser = DLParserManager()
+    
+    // Search objects
+    var dataArray = [String]()
+    var filteredArray = [String]()
+    var shouldShowSearchResults = false
+    var searchController: UISearchController!
+    var customSearchController: CustomSearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +32,61 @@ class DetailsViewController: UIViewController, UITableViewDataSource, DLParserMa
         //Automatic tvc height
         self.tableView.estimatedRowHeight = 120;
         self.tableView.rowHeight = UITableViewAutomaticDimension;
-        
+       
+        //Setup Search - Bar
+        configureCustomSearchController()
+
     }
     
     override func viewWillAppear(animated: Bool) {
         //Service request Method
         invokeServiceToFetchData()
     }
+
+    
+    func configureCustomSearchController() {
+        customSearchController = CustomSearchController(searchResultsController: self, searchBarFrame: CGRectMake(0.0, 0.0, tableView.frame.size.width, 50.0), searchBarFont: UIFont(name: "Futura", size: 16.0)!, searchBarTextColor: UIColor.orangeColor(), searchBarTintColor: UIColor.blackColor())
+        
+        customSearchController.customSearchBar.placeholder = "Search in this awesome bar..."
+        tableView.tableHeaderView = customSearchController.customSearchBar
+        
+        customSearchController.customDelegate = self
+    }
+    
+    // MARK: CustomSearchControllerDelegate functions
+    
+    func didStartSearching() {
+        shouldShowSearchResults = true
+        tableView.reloadData()
+    }
+    
+    
+    func didTapOnSearchButton() {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tableView.reloadData()
+        }
+    }
+    
+    
+    func didTapOnCancelButton() {
+        shouldShowSearchResults = false
+        tableView.reloadData()
+    }
+    
+    
+    func didChangeSearchText(searchText: String) {
+        // Filter the data array and get only those countries that match the search text.
+        filteredArray = dataArray.filter({ (country) -> Bool in
+            let countryText: NSString = country
+            
+            return (countryText.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch).location) != NSNotFound
+        })
+        
+        // Reload the tableview.
+        tableView.reloadData()
+    }
+
     
     func invokeServiceToFetchData(){
         let parserManager = DLParserManager()
@@ -64,7 +119,12 @@ class DetailsViewController: UIViewController, UITableViewDataSource, DLParserMa
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrData.count
+        if shouldShowSearchResults {
+            return filteredArray.count
+        }
+        else {
+            return arrData.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
