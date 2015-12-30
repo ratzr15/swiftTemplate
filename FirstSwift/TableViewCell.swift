2e761988c66d2d732d942ffb1e65013ac1cb6c56
Rathish <rathish_citys@eres.ae>
2015-12-30 10:07:06 +0400

    Clean up

---
+++ FirstSwift/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  ERESTemplate
+//
+//  Created by Rathish on 28/12/15.
+//  Copyright © 2015 DubaiLandDept. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
